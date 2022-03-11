aa56cd7cb790266b2d0a150114b627fc100a4993
Beddok <abidfajar50@gmail.com>
2022-03-11 15:49:58 +0700

    feat: menambahkan featur lifes

--- snake-game/index.js
+++ snake-game/index.js
@@ -42,13 +42,31 @@ let apple1 = {
     position: initPosition(),
 }
 let apple2 = {
-    color: "red",
+    color: "yellow",
     position: initPosition(),
+}
+let lifes = {
+    color: "blue",
+    position: initPosition(),
+    lifes: 3,
 }
 
 function drawCell(ctx, x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+}
+
+function drawlifes(snake){
+    let lifesCanvas;
+    if (lifes.color == lifes.color) {
+        lifesCanvas = document.getElementById("score3Board");
+    } 
+    let lifesCtx = lifesCanvas.getContext("2d");
+
+    lifesCtx.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+    lifesCtx.font = "25px Arial";
+    lifesCtx.fillStyle = lifes.color
+    lifesCtx.fillText("Life: \n" + lifes.lifes, 10, lifesCanvas.scrollHeight / 2);
 }
 
 function drawScore(snake) {
@@ -80,8 +98,10 @@ function draw() {
         }
         drawCell(ctx, apple1.position.x, apple1.position.y, apple1.color);
         drawCell(ctx, apple2.position.x, apple2.position.y, apple2.color);
+        drawCell(ctx, lifes.position.x, lifes.position.y, lifes.color);
 
         drawScore(snake);
+        drawlifes(snake);
     }, REDRAW_INTERVAL);
 }
 
@@ -100,7 +120,7 @@ function teleport(snake) {
     }
 }
 
-function eat(snake, apple1, apple2) {
+function eat(snake, apple1, apple2, lifes) {
     if (snake.head.x == apple1.position.x && snake.head.y == apple1.position.y) {
         apple1.position = initPosition();
         snake.score++;
@@ -113,30 +133,37 @@ function eat(snake, apple1, apple2) {
         //this
         snake.body.push({x: snake.head.x, y: snake.head.y});
     }
+    if (snake.head.x == lifes.position.x && snake.head.y == lifes.position.y) {
+        lifes.position = initPosition();
+        lifes.lifes++;
+            
+        //this
+        snake.body.push({x: snake.head.x, y: snake.head.y});
+    }
 }
 
 function moveLeft(snake) {
     snake.head.x--;
     teleport(snake);
-    eat(snake, apple1, apple2);
+    eat(snake, apple1, apple2, lifes);
 }
 
 function moveRight(snake) {
     snake.head.x++;
     teleport(snake);
-    eat(snake, apple1, apple2);
+    eat(snake, apple1, apple2, lifes);
 }
 
 function moveDown(snake) {
     snake.head.y++;
     teleport(snake);
-    eat(snake, apple1, apple2);
+    eat(snake, apple1, apple2, lifes);
 }
 
 function moveUp(snake) {
     snake.head.y--;
     teleport(snake);
-    eat(snake, apple1, apple2);
+    eat(snake, apple1, apple2, lifes);
 }
 
 function move(snake) {
